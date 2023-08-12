5b4ab19fabce28ad297d9afca54d5005b4f6b728
varun576 <varunlakshmanan171@gmail.com>
2023-08-12 22:40:55 +0530

    Add files via upload

---
+++ DAY-9_C#(ENTITY FRAMEWORK)_10-08-2023/EFDBF/CRUDEmp (1).cs
@@ -0,0 +1,48 @@
+﻿using EFDBF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDBF
+{
+    internal class CRUDEmp
+    {
+        public void InsertRecords()
+        {
+            using (var context = new EmployeeMgmtContext())
+            {
+               
+
+                var emp = new Emp()
+                {
+                    Empno = 123,
+                    Empname = "Suriya",
+                    Deptno = 1
+                };
+                context.Emps.Add(emp);
+                context.SaveChanges();
+
+
+            }
+        }
+        public void UpdateRecords()
+        {
+            var context = new EmployeeMgmtContext();
+            var selectedEmp = context.Emps.FirstOrDefault(d => d.Deptno == 1);
+            selectedEmp.Empname = "Suriyalakshmi";
+            context.SaveChanges();
+        }
+        public void DeleteRecord()
+        {
+            var context = new EmployeeMgmtContext();
+
+            var selectedEmp = context.Depts.FirstOrDefault(d => d.Deptno == 1);
+            context.Depts.Remove(selectedEmp);
+            context.SaveChanges();
+        }
+        
+    }
+}
