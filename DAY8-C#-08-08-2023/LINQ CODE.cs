2ee15944b76574639d5b5b4a2e7463b8bac7b300
varun576 <varunlakshmanan171@gmail.com>
2023-08-14 14:52:53 +0530

    Add files via upload

---
+++ DAY8-C#-08-08-2023/LINQ CODE.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProgram
+{
+    internal class LINQDemo
+    {
+        public void ShowResults()
+        {
+            // Data source.
+            int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+            // Query Expression.
+            int highScoreCount = (
+            from score in scores
+            where score > 80
+            select score).Count();
+            Console.WriteLine(highScoreCount);
+
+
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                orderby score descending
+                select score;
+
+            // Execute the query to produce the results
+            foreach (int testScore in scoreQuery)
+            {
+                Console.WriteLine(testScore);
+            }
+
+            IEnumerable<int> highScoresQuery =
+           from score in scores
+           where score > 80
+           select score;
+            int scoreCount = highScoresQuery.Count();
+            Console.WriteLine(scoreCount);
+
+        }
+    }
+
+}
