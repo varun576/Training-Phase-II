790d640a33bf3aed5955eed79c2c4046a8fb8f75
varun576 <varunlakshmanan171@gmail.com>
2023-08-14 15:00:15 +0530

    Add files via upload

---
+++ DAY8-C#-08-08-2023/GenericCollectionsDemo.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProgram
+{
+    internal class GenericCollectionsDemo
+    {
+        public void ListElements()
+        {
+            List<string> numbers = new List<string>();
+            numbers.Add("343");
+            numbers.Add("35");
+            numbers.Add("21");
+
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            numbers.Sort();
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            numbers.Remove("230");
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public void StackElements()
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(34);
+            numbers.Push(423);
+            numbers.Push(300);
+            numbers.Push(24);
+            numbers.Push(8);
+            Console.WriteLine(numbers.Average());
+            Console.WriteLine(numbers.Sum());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Pop" + numbers.Pop());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            numbers.Clear();
+            Console.WriteLine("Pop" + numbers.Pop());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public void QueueElements()
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(43);
+            numbers.Enqueue(431);
+            numbers.Enqueue(-123);
+            numbers.Enqueue(2);
+            numbers.Enqueue(65);
+            //Console.WriteLine(numbers.Average());
+            //Console.WriteLine(numbers.Sum());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Pop" + numbers.Dequeue());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            //       numbers.Clear();
+            Console.WriteLine("Pop" + numbers.Dequeue());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(numbers.First());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public void HashElements() 
+        {
+            Dictionary<string, string> students =
+                new Dictionary<string, string>();
+            students.Add("rno", "100");
+            students.Add("name", "Varun");
+            foreach ( string k in students.Keys)
+            { 
+                Console.WriteLine(k + " : "
+                    + students[k]);
+            } 
+        }
+        public void SortedListElements()
+        {
+            SortedList<string, string> students =
+                new SortedList<string, string>();
+            students.Add("rno", "100");
+            students.Add("name", "Varun");
+            foreach (string k in students.Keys)
+            {
+                Console.WriteLine(k + " : "
+                    + students[k]);
+            }
+        }
+
+
+    }
+}
---
+++ DAY8-C#-08-08-2023/NonGenericCollectionsDemo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProgram
+{
+    internal class NonGenericCollectionsDemo
+    {
+        public void ArrayListElements()
+        {
+            ArrayList numbers = new ArrayList();
+            numbers.Add("hi");
+            numbers.Add(241);
+            numbers.Add(12);
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            numbers.Remove("241");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public void StackElements()
+        {
+            Stack numbers = new Stack();
+            numbers.Push(100);
+            numbers.Push(287);
+            numbers.Push(-100);
+            numbers.Push(333);
+            numbers.Push(8765);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Pop" + numbers.Pop());
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            numbers.Clear();
+            Console.WriteLine("Pop" + numbers.Pop());
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public void QueueElements()
+        {
+            Queue numbers = new Queue();
+            numbers.Enqueue(100);
+            numbers.Enqueue(287);
+            numbers.Enqueue(-100);
+            numbers.Enqueue(333);
+            numbers.Enqueue(8765);
+            //Console.WriteLine(numbers.Average());
+            //Console.WriteLine(numbers.Sum());
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Pop" + numbers.Dequeue());
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            //       numbers.Clear();
+            Console.WriteLine("Pop" + numbers.Dequeue());
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(numbers);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public void HashElements()
+        {
+            Hashtable students =
+                new Hashtable();
+            students.Add("rno", "12");
+            students.Add("name", "Varun");
+            foreach (var k in students.Keys)
+            {
+                Console.WriteLine(k + " : "
+                    + students[k]);
+            }
+        }
+        public void SortedListElements()
+        {
+            SortedList students =
+                new SortedList();
+            students.Add("rno", "12");
+            students.Add("name", "Varun");
+            foreach (string k in students.Keys)
+            {
+                Console.WriteLine(k + " : "
+                    + students[k]);
+            }
+        }
+
+    }
+}
